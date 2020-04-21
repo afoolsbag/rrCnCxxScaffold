6e3f366c870fda3b9633649f24de14249f40489a
zhengrr <afoolsbag@gmail.com>
2020-04-21 14:00:32 +0800

    feat: 更新 .gitmodules-update.cmd 脚本

--- .gitmodules-update.cmd
+++ .gitmodules-update.cmd
@@ -1,23 +1,23 @@
 @ECHO OFF
 CHCP 65001
 SETLOCAL ENABLEEXTENSIONS
-SET script_directory=%~dp0%
+SET script_directory=%~dp0
 
 WHERE /Q git ^
         || ECHO The git executable not found. && ^
-           CALL :pause_if_need && ^
+           CALL :pause_if_double_click && ^
            EXIT /B 1
 
 git submodule update --remote %script_directory% ^
         && ECHO Update git submodules succeed. ^
         || ECHO Update git submodules failed. && ^
-           CALL :pause_if_need && ^
+           CALL :pause_if_double_click && ^
            EXIT /B 2
 
-CALL :pause_if_need
+CALL :pause_if_double_click
 EXIT /B 0
 
-:pause_if_need
+:pause_if_double_click
         ECHO %CMDCMDLINE% | FINDSTR /L %COMSPEC% 1>NUL 2>NUL ^
                 && PAUSE
-EXIT /B 0
+        EXIT /B 0
